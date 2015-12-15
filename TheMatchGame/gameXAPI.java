6ec0e82dc1407db2b45c713c5479640f5f51ba47
Freddy Coa <fredsomer@gmail.com>
2015-12-15 23:30:56 +0100

    Update gameXAPI.java

--- TheMatchGame/gameXAPI.java
+++ TheMatchGame/gameXAPI.java
@@ -2,9 +2,9 @@
 
  
   wbXAPI = (WebView) findViewById(R.id.webView);
-  wbXAPI.getSettings().setJavaScriptEnabled(true);
+  wbXAPI.getSettings().setJavaScriptEnabled(true); // Habilitamos el JS para poder ejecutar funciones remotas.
   wbXAPI.getSettings().getJavaScriptEnabled();
-  wbXAPI.loadUrl("http://192.168.1.100:8080/AndroidDB/badgeMobile.php");
+  wbXAPI.loadUrl("http://192.168.1.100:8080/AndroidDB/badgeMobile.php"); // url de la Pagina Web que contiene las funciones JS para la Emision del Badge
   
   // Desde la linea 4 a 7 es recomendable colocarlo dentro del OnCreate, haciendo esto la lectura de url ya esta cargada al entrar a la
   // actividad que emitira el Badge
