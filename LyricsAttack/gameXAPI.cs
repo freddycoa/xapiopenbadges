7ba878a6712b6e5333826858509d0aaf6514a870
Freddy Coa <fredsomer@gmail.com>
2015-12-16 15:46:14 +0100

    Update gameXAPI.cs

--- LyricsAttack/gameXAPI.cs
+++ LyricsAttack/gameXAPI.cs
@@ -1,13 +1,13 @@
-# xapiopenbadges
+// xapiopenbadges
 // Emision de Badges
 
 
       Application.LoadLevel ("_Escenariofinal"); // al cumplir la condicion de aprobación se lee el escenario final
-
-			string url = "http://localhost:8080/AndroidDB/BadgeMobile.php?var1=value2&amp;var2=value2"; 
-			// Asignamos la url y las variables que se enviaran a la plataforma web que posea las Funciones Javascript xAPI
-			
-			WWW www = new WWW(url);
-			StartCoroutine(WaitForRequest(www));
-			
-			// de esta forma hacemos el llamado de la funcion XAPI y envio de datos que se cargaran dentro del Badge
+      
+	string url = "http://localhost:8080/AndroidDB/BadgeMobile.php?var1=value2&amp;var2=value2"; 
+	// Asignamos la url y las variables que se enviaran a la plataforma web que posea las Funciones Javascript xAPI
+	
+	WWW www = new WWW(url);
+	StartCoroutine(WaitForRequest(www));
+	
+	// de esta forma hacemos el llamado de la funcion XAPI y envio de datos que se cargaran dentro del Badge
